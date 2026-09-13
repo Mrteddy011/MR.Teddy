a9b29c51e269ae10179f55d595127392d645056e
Mrteddy011 <sineth2001001@gmail.com>
2026-09-13 11:56:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Mrteddy011/MR.Teddy/blob/main/images/MR.Teddy.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MR.Teddy Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94761380228',  // Replace with the owner's phone number
 
 
 
